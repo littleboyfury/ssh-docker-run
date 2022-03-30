33c3009ebc0ba6dfe06b71b69aeba41de1dd4260
skyfury <xj.zhang@maimemo.com>
2022-03-30 17:42:23 +0800

    test ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,8 @@ cat id_rsa
 chmod 700 id_rsa
 #apk add openssh
 echo "${INPUT_SERVER_HOST}"
-#ssh -o StrictHostKeyChecking=no -i id_rsa ${INPUT_SERVER_HOST} "
-#          docker login ${INPUT_DOCKER_HOST} -u ${INPUT_DOCKER_USERNAME} -p ${INPUT_DOCKER_PASSWORD}
-#          docker pull ${INPUT_DOCKER_HOST}/${INPUT_DOCKER_NAMESPACE}/${INPUT_DOCKER_REPO}:${INPUT_IMAGE_TAG}
-#          docker stop ${INPUT_DOCKER_REPO}
-#          docker run -itd --rm --name ${INPUT_DOCKER_REPO} -p ${INPUT_CONTAINER_PORT} ${INPUT_DOCKER_HOST}/${INPUT_DOCKER_NAMESPACE}/${INPUT_DOCKER_REPO}:${INPUT_IMAGE_TAG}"
+ssh -o StrictHostKeyChecking=no -i id_rsa ${INPUT_SERVER_HOST} "
+          docker login ${INPUT_DOCKER_HOST} -u ${INPUT_DOCKER_USERNAME} -p ${INPUT_DOCKER_PASSWORD}
+          docker pull ${INPUT_DOCKER_HOST}/${INPUT_DOCKER_NAMESPACE}/${INPUT_DOCKER_REPO}:${INPUT_IMAGE_TAG}
+          docker stop ${INPUT_DOCKER_REPO}
+          docker run -itd --rm --name ${INPUT_DOCKER_REPO} -p ${INPUT_CONTAINER_PORT} ${INPUT_DOCKER_HOST}/${INPUT_DOCKER_NAMESPACE}/${INPUT_DOCKER_REPO}:${INPUT_IMAGE_TAG}"
